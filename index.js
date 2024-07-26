78226df9bc7ab4732eefa7746d728e23a8db699a
pedrocarvalho3 <pdrpvp0310@gmail.com>
2024-07-26 11:54:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const path = require("path");
+
+const app = express();
+
+dotenv.config();
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "chave-secreta",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("Página inicial");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (
+    username === process.env.ADMIN_USERNAME &&
+    password === process.env.ADMIN_PASSWORD
+  ) {
+    req.session.loggedIn = true;
+    console.log("Usuário logado");
+    return res.redirect("/admin");
+  } else {
+    console.log("erro");
+    res.redirect("/login");
+  }
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});

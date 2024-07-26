830a9aed158f3fb75afdc73302229fd45f4e1ef9
pedrocarvalho3 <pdrpvp0310@gmail.com>
2024-07-26 14:01:02 -0300

    criação da página administrativa e estilização

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const session = require("express-session");
 const dotenv = require("dotenv");
 const path = require("path");
 const { body, validationResult } = require("express-validator");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -12,15 +13,25 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(
   session({
     secret: "chave-secreta",
     resave: false,
     saveUninitialized: false,
+    cookie: { maxAge: 60000 },
   })
 );
 
 app.use(express.static(path.join(__dirname, "public")));
+
+function authVerify(req, res, next) {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+}
 
 app.get("/", (req, res) => {
   res.send("Página inicial");
@@ -48,14 +59,26 @@ app.post(
       password === process.env.ADMIN_PASSWORD
     ) {
       req.session.loggedIn = true;
+      res.cookie("loggedIn", true, { maxAge: 60000 });
+      console.log("Usuário logado");
       return res.redirect("/admin");
     } else {
-      res.render("login", { errors: [{ msg: "Usuário ou senha inválidos" }] });
+      return res.status(401).send("Usuário ou senha incorretos");
     }
   }
 );
 
-const port = process.env.PORT;
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.clearCookie("loggedIn");
+  res.redirect("/login");
+});
+
+app.get("/admin", authVerify, (req, res) => {
+  res.render("admin");
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
